2bc7e382ea7da90cd9fee8874d1e7e4fd8c724c9
almeida_gabriel <almeida_gabriel@sicredi.com.br>
2021-08-15 19:52:43 -0300

    Initial commit

---
+++ src/main/java/com/apus/drones/apusdronesbackend/ApusDronesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.apus.drones.apusdronesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApusDronesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApusDronesBackendApplication.class, args);
+	}
+
+}
