108878b9c721661d41d7a8ec0906b5c7cf7a61c1
@gabriel.rabelo <gabriel.rabelo@edu.pucrs.br>
2021-08-15 20:46:26 -0300

    swagger-config | faz configuração inicial do swagger

---
+++ src/main/java/com/apus/drones/apusdronesbackend/config/SwaggerUiWebMvcConfigurer.java
@@ -0,0 +1,33 @@
+package com.apus.drones.apusdronesbackend.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SwaggerUiWebMvcConfigurer implements WebMvcConfigurer {
+
+    private final String baseUrl;
+
+    public SwaggerUiWebMvcConfigurer(@Value("${application.base.url}") String baseUrl) {
+        this.baseUrl = baseUrl;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        var baseUrl = StringUtils.trimTrailingCharacter(this.baseUrl, '/');
+        registry.
+                addResourceHandler(baseUrl + "/swagger-ui/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/springfox-swagger-ui/")
+                .resourceChain(false);
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController(baseUrl + "/swagger-ui/")
+                .setViewName("forward:" + baseUrl + "/swagger-ui/index.html");
+    }
+}
