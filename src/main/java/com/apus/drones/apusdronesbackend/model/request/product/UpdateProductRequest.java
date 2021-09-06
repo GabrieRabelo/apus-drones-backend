4103eb1055fc0a06838896d10a872a9a0ecfceee
Felipe Bordignon <felipe.bordignon@southsystem.com.br>
2021-09-06 19:17:30 -0300

    endpoints de produto (nao levando em conta idusuario ainda)

---
+++ src/main/java/com/apus/drones/apusdronesbackend/model/request/product/UpdateProductRequest.java
@@ -0,0 +1,21 @@
+package com.apus.drones.apusdronesbackend.model.request.product;
+
+import com.apus.drones.apusdronesbackend.model.enums.ProductStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+public class UpdateProductRequest {
+    @NotNull(message = "ID do produto não pode ser nulo ou vazio")
+    private Long productId;
+
+    private String name;
+    private BigDecimal price;
+    private ProductStatus status;
+    private Double weight;
+}
--- src/main/java/com/apus/drones/apusdronesbackend/service/ProductServiceImpl.java
+++ src/main/java/com/apus/drones/apusdronesbackend/service/ProductServiceImpl.java
@@ -1,12 +1,14 @@
 package com.apus.drones.apusdronesbackend.service;
 
 import com.apus.drones.apusdronesbackend.model.entity.ProductEntity;
-import com.apus.drones.apusdronesbackend.model.request.CreateProductRequest;
+import com.apus.drones.apusdronesbackend.model.request.product.CreateProductRequest;
+import com.apus.drones.apusdronesbackend.model.request.product.UpdateProductRequest;
 import com.apus.drones.apusdronesbackend.repository.ProductRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 @Slf4j
@@ -27,5 +29,51 @@ public class ProductServiceImpl implements ProductService {
         log.info("Saved new product entity with id [{}]", generatedId);
 
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @Override
+    public ResponseEntity<ProductEntity> get(Long id) {
+        return productRepository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Não foi possível encontrar o produto com ID " + id));
+    }
+
+    @Override
+    public ResponseEntity<Void> update(UpdateProductRequest request) {
+        ProductEntity entity =
+                productRepository.findById(request.getProductId())
+                        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Não foi possível encontrar o produto com ID " + request.getProductId()));
+
+        updateProduct(request, entity);
+
+        productRepository.save(entity);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<Void> delete(Long id) {
+        ProductEntity entity =
+                productRepository.findById(id)
+                        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Não foi possível encontrar o produto com ID " + id));
+
+        productRepository.delete(entity);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+
+    private void updateProduct(UpdateProductRequest request, ProductEntity entity) {
+        if (request.getName() != null)
+            entity.setName(request.getName());
+
+        if (request.getPrice() != null)
+            entity.setPrice(request.getPrice());
+
+        if (request.getStatus() != null)
+            entity.setStatus(request.getStatus());
+
+        if (request.getWeight() != null)
+            entity.setWeight(request.getWeight());
     }
 }
