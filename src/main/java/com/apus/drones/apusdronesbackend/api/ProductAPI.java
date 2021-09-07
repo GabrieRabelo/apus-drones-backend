69c82096e804655932d49a740fccabbc4a17c61e
@gabriel.rabelo <gabriel.rabelo@edu.pucrs.br>
2021-09-07 15:03:53 -0300

    ajusta controller

--- src/main/java/com/apus/drones/apusdronesbackend/api/ProductAPI.java
+++ src/main/java/com/apus/drones/apusdronesbackend/api/ProductAPI.java
@@ -1,8 +1,29 @@
 package com.apus.drones.apusdronesbackend.api;
 
 import com.apus.drones.apusdronesbackend.model.request.CreateProductRequest;
+import com.apus.drones.apusdronesbackend.service.ProductService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-public interface ProductAPI {
-    ResponseEntity<Void> create(CreateProductRequest request);
+@RestController
+@RequestMapping("/api/v1/product")
+@Slf4j
+public class ProductAPI {
+    private final ProductService productService;
+
+    public ProductAPI(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> create(@RequestBody @Validated CreateProductRequest request) {
+        log.info("Received a new create product request for product name [{}]", request.getName());
+
+        return productService.create(request);
+    }
 }
