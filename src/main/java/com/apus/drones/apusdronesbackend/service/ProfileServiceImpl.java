daaeb135debb1c08fd5436977dc747da8f1ca00a
Ardel_Nunes <Ardel_Nunes@Dellteam.com>
2021-10-21 02:49:37 -0300

    Update user

--- src/main/java/com/apus/drones/apusdronesbackend/service/ProfileServiceImpl.java
+++ src/main/java/com/apus/drones/apusdronesbackend/service/ProfileServiceImpl.java
@@ -15,6 +15,8 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.persistence.EntityNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -23,10 +25,12 @@ public class ProfileServiceImpl implements ProfileService {
 
     private final UserRepository userRepository;
     private final AddressRepository addressRepository;
+    private final PointCreatorService pointCreatorService;
 
-    public ProfileServiceImpl(UserRepository userRepository, AddressRepository addressRepository) {
+    public ProfileServiceImpl(UserRepository userRepository, AddressRepository addressRepository, PointCreatorService pointCreatorService) {
         this.userRepository = userRepository;
         this.addressRepository = addressRepository;
+        this.pointCreatorService = pointCreatorService;
     }
     @Override
     public UserDTO getById() {
@@ -43,5 +47,70 @@ public class ProfileServiceImpl implements ProfileService {
         } else {
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Falha ao carregar dados do perfil. Usuário não autenticado.");
         }
+    }
+
+    @Override
+    public UserDTO update (UserDTO userDTO){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        if(auth.isAuthenticated()) {
+            CustomUserDetails details = (CustomUserDetails) auth.getPrincipal();
+
+            UserEntity entity = userRepository.findById(details.getUserID()).orElse(null);
+
+            List<AddressDTO> addressDTOList = new ArrayList<>();
+            updateUser(userDTO, entity, addressDTOList);
+            UserEntity savedUserEntity = userRepository.save(entity);
+            return UserDTOMapper.fromUserEntity(savedUserEntity, addressDTOList);
+        } else {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Usuário não autenticado.");
+        }
+    }
+
+    private void updateUser(UserDTO userDTO, UserEntity entity, List<AddressDTO> addressDTOList) {
+        if (userDTO.getName() != null) {
+            entity.setName(userDTO.getName());
+        }
+
+        if (userDTO.getEmail() != null) {
+            entity.setEmail(userDTO.getEmail());
+        }
+
+        if (userDTO.getCpfCnpj() != null) {
+            entity.setCpfCnpj(userDTO.getCpfCnpj());
+        }
+
+        if (userDTO.getAvatarUrl() != null) {
+            entity.setAvatarUrl(userDTO.getAvatarUrl());
+        }
+
+        if (userDTO.getAddresses() != null) {
+            updateAddress(userDTO, addressDTOList);
+        }
+    }
+
+    private void updateAddress (UserDTO userDTO, List<AddressDTO> addressDTOList) {
+        userDTO.getAddresses().forEach(address -> {
+            if(address.getId() == null){
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Para atualizar um endereço, é necessário inserir o ID do endereço.");
+            }
+            try {
+                AddressEntity finalAddress = addressRepository.getById(address.getId());
+                if(address.getDescription() != null){
+                    finalAddress.setDescription(address.getDescription());
+                }
+                if(address.getLat() != null || address.getLng() != null){
+                    finalAddress.setCoordinates(pointCreatorService.createPoint(address.getLat() != null ? address.getLat() : finalAddress.getCoordinates().getX(),
+                            address.getLng() != null ? address.getLng() : finalAddress.getCoordinates().getY()));
+                }
+                if(address.getNumber() != null){
+                    finalAddress.setNumber(address.getNumber());
+                }
+                addressRepository.save(finalAddress);
+                addressDTOList.add(AddressDTOMapper.fromAddressEntity(finalAddress));
+            } catch (EntityNotFoundException e){
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Para atualizar um endereço, é necessário inserir um ID de endereço válido.");
+            }
+        });
     }
 }
