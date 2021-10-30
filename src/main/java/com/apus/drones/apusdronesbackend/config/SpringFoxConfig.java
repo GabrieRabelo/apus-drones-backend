c10842381a81a3cc6e4ff5a3f1bb2fa994b353b2
Calebe Rocha <angelo.calebe@edu.pucrs.br>
2021-10-29 22:03:47 -0300

    Swagger habilitado no SpringFoxConfig; Cadeado removido de /authenticate/*

--- src/main/java/com/apus/drones/apusdronesbackend/config/SpringFoxConfig.java
+++ src/main/java/com/apus/drones/apusdronesbackend/config/SpringFoxConfig.java
@@ -2,9 +2,11 @@ package com.apus.drones.apusdronesbackend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.AuthorizationScopeBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
@@ -16,19 +18,28 @@ import java.util.List;
 import static springfox.documentation.service.ApiInfo.DEFAULT_CONTACT;
 
 @Configuration
+@EnableOpenApi
 public class SpringFoxConfig {
 
     @Bean
     public Docket api() {
         AuthorizationScope[] authScopes = new AuthorizationScope[]{
-                new AuthorizationScopeBuilder().scope("global").description(
-                        "full access").build()};
-        SecurityReference securityReference = SecurityReference.builder().reference(
-                "JWT").scopes(authScopes).build();
+                new AuthorizationScopeBuilder().scope("global").description("full access").build()
+        };
+
+        SecurityReference securityReference = SecurityReference
+                .builder()
+                .reference("JWT")
+                .scopes(authScopes)
+                .build();
 
         List<SecurityContext> securityContexts = List.of(
-                SecurityContext.builder().securityReferences(List.of(
-                        securityReference)).build());
+                SecurityContext.builder().securityReferences(
+                        List.of(securityReference)
+                )
+                .operationSelector(o -> o.requestMappingPattern().matches("^(?:(?!/api/v\\d+/authenticate/.*).)*$"))
+                .build()
+        );
 
         HttpAuthenticationScheme authenticationScheme = HttpAuthenticationScheme
                 .JWT_BEARER_BUILDER
@@ -37,7 +48,7 @@ public class SpringFoxConfig {
 
         return new Docket(DocumentationType.OAS_30)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                 .paths(PathSelectors.any())
                 .build()
                 .securitySchemes(Collections.singletonList(authenticationScheme))
@@ -48,6 +59,6 @@ public class SpringFoxConfig {
             "Apus Drones",
             "Aplicativo de delivery por drones!", "1.0-SNAPSHOT",
             "urn:tos", DEFAULT_CONTACT,
-            "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0", Collections.emptyList());
-
+            "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0", Collections.emptyList()
+    );
 }
